e48ebc092f1cb0dbf7d33ffdb31cd2dc48c7c910
mateobou12 <boumateo12@gmail.com>
2021-01-19 21:02:34 -0300

    mercado liebre 1.3

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
 const express = require('express');
 const app = express();
 
-// Método para formatear el puerto en base al argumento enviado en npm start
-const formattedPort = (() => {
-    // se destructura el array para que tome la tercera posición 
-    // (process.argv es un array que tiene como primer index el PATH de node y como segundo índice el archivo)
-    let [,,portNumber] = process.argv;
-    // Si el argumento del puerto es enviado y al parsearlo a número no es NAN, se devuelve
-    // Caso contrario el puerto por defecto es 3000
-    return portNumber && !isNaN(parseInt(portNumber)) ? parseInt(portNumber) : 3030;
-})()
+app.listen(process.env.PORT || 3000,()=> console.log("inicio del servidor"));
 
-// Se define la carpeta pública para exponer assets y archivos estáticos
-app.use(express.static('public'));
+const path=require("path");
 
-// Se levanta el servidor en el resultado de parsear el puerto desde el argumento
-app.listen(formattedPort, () =>{
-    console.log('Servidor funcionando en puerto ' + formattedPort);
-});
+app.use(express.static(path.resolve(__dirname,"public")));
 
-// Se definen rutas y respuestas en el mismo script (recordar que esto puede modularizarse)
+app.post("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","home.html")));
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
-});
+app.post("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views","login.html")));
 
-app.post('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
-});
+app.post("/register",(req,res)=> res.sendFile(path.resolve(__dirname,"views","register.html")));
 
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
+app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","home.html")));
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
+app.get("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views","login.html")));
+
+app.get("/register",(req,res)=> res.sendFile(path.resolve(__dirname,"views","register.html")));
